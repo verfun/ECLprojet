f6717ea434df25b09a1618399e98f3606758d5b5
SafoineMoncefAmine <safoinemoncefamine@gmail.com>
2016-12-29 18:34:22 +0000

    add lines that ansert new employees

--- CompAerBeta/src/main/java/com/ECL/CompAerBetaApplication.java
+++ CompAerBeta/src/main/java/com/ECL/CompAerBetaApplication.java
@@ -1,11 +1,17 @@
 package com.ECL;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.ECL.Domain.Aeroport;
+import com.ECL.Domain.EmployeeNav;
+import com.ECL.Domain.Licence;
+import com.ECL.Domain.Pilote;
 import com.ECL.Repository.AeroportRepository;
+import com.ECL.Repository.EmployeeRepository;
 
 @SpringBootApplication
 public class CompAerBetaApplication {
@@ -14,11 +20,14 @@ public class CompAerBetaApplication {
 		ApplicationContext ctx = SpringApplication.run(CompAerBetaApplication.class, args);
 		
 		AeroportRepository aeRepository = ctx.getBean(AeroportRepository.class);
-		aeRepository.save(new Aeroport("casablanca","MD5"));
-		aeRepository.save(new Aeroport("lyon","LYS"));
-		aeRepository.save(new Aeroport("london","LDN"));
-		
-		
+		aeRepository.save(new Aeroport("casablanca","MD5","casablanca"));
+		aeRepository.save(new Aeroport("lyon","LYS","lyon"));
+		aeRepository.save(new Aeroport("london","LDN","london"));
+		EmployeeRepository emRepository = ctx.getBean(EmployeeRepository.class);
+		emRepository.save(new EmployeeNav("celen","garcia",new Date()
+				,"00341","hotesse",0));
+		emRepository.save(new Pilote("Rizki","Lahcen",new Date()
+				,"00341",0,new Licence("2301")));
 		aeRepository.findAll().forEach(p->System.out.println("aeroport "+p.getName()+" => "+p.getCode()));
 		
 
